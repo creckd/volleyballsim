3702e3b4bd1027993e969c0347ab8b4d81f209a4
Creckd <daniel.nagy@invictus.com>
2019-02-27 15:10:48 +0100

    fikkkkszü

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+
+	private static InputController instance = null;
+	public static InputController Instance {
+		get {
+			if (instance == null)
+				instance = FindObjectOfType<InputController>();
+			return instance;
+		}
+	}
+
+	public Action<int> TapHappened = delegate { };
+	public Action<int> ReleaseHappened = delegate { };
+
+	private void Update() {
+		// PC
+#if UNITY_EDITOR
+		if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)) {
+			TapHappened(0);
+		}
+		if (Input.GetMouseButtonUp(0) || Input.GetKeyUp(KeyCode.Space)) {
+			ReleaseHappened(0);
+		}
+#endif
+#if UNITY_ANDROID && !UNITY_EDITOR
+				Touch[] touches = Input.touches;
+		foreach (var touch in touches) {
+			if (touch.phase == TouchPhase.Began) {
+				TapHappened(touch.fingerId);
+			}
+			if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled) {
+				ReleaseHappened(touch.fingerId);
+			}
+		}
+#endif
+	}
+}
