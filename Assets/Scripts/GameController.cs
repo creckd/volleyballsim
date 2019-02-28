efc9af9f936cc55af88c4a2e43916834a9dbf8d0
Creckd <daniel.nagy@invictus.com>
2019-02-28 12:16:44 +0100

    getting ready for mobile test

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,18 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private void Awake() {
+		InputController.Instance.TapHappened += TapHappened;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void TapHappened(int fingerID) {
+	}
 }
